8082bba2962af10c03f29c8d10862d8577d1a916
therealjohnwall1 <andyvvan@gmail.com>
2024-04-07 16:44:54 -0700

    add schema design

--- backend/dbConnector.py
+++ backend/dbConnector.py
@@ -3,39 +3,61 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 import os
-from pydantic import BaseModel
+# from pydantic import BaseModel
 
 load_dotenv()
-
 # Provides connector to specific database->collection
 
+"""
+Schema design for data
+
+- one collection, users and tasks inside
+- users and tasks will share a undirected link
+    multiple users can "own" one task
+    to display tasks for a user we just pull from array user has(make sure to limit)
+"""
+
 class Connector:
-    def __init__(self, database: str, collection: str):
+    def __init__(self, db_name: str, collect_name: str):
         uri = os.getenv("SERVER_URI")
-        self.client = MongoClient(uri, server_api = ServerApi('1'))
+        self.client = MongoClient(uri)
+        
         try:
             self.client.admin.command('ping')
             print("Sucessfully connected to database")
         except Exception as e:
             print(e)
         
-        self.db = self.client.database
-        self.collection = self.db.collection
-    
-    # Updating document to our collection 
+        self.db = self.client[db_name]
+        self.collection = self.db[collect_name]
 
+
+    # Updating document to our collection 
     def add_task(self, text: str):
         # Date is in utc format
-
         date = datetime.datetime.now()
         doc = {
             "timestamp" : date,
             "text" : text
         }
-        self.collection.insert_one(doc)
 
+        id = self.collection.insert_one(doc).inserted_id
 
-    # def delete_task(key):
+        print(f"Added {id}")
+
+    def delete_task(self, text: str):
+        self.collection.find_one_and_delete({"text": text})
+
+    def wipe(self):
+        self.collection.delete_many({})
+    
+    # Will iterate through entire collection, place in array and return 
+    def pull(self):
+        pass
+
+        
+
+        
 
 
 
--- backend/taskLinker.py
+++ backend/taskLinker.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 import os
 from pydantic import BaseModel
 
+# Meaning the key for the body will be called <task_body>
+# need to query them in order for iteration 
+
 class Task(BaseModel):
     task_body : str
 
@@ -18,15 +21,22 @@ async def root():
     return {"Home":"Home"} 
 
 # adding new task
-@app.put("/send")
+@app.put("/add")
 async def add_document(task: Task):
     if task:
         connector.add_task(task.task_body)
         return {"message": f"{task.task_body} added to database"}
     else:
+        print("db fails")
         raise HTTPException(status_code=422, detail="Invalid Item")
 
+
 # Add ID system later to delete faster
-@app.delete("/delete/{task_body}")
-def del_document(task_body : str):
-    connector.del_task(task_body)
+@app.delete("/delete")
+def del_document(task : Task):
+    connector.delete_task(task.task_body)
+
+
+@app.delete("/wipe")
+def wipe_document():
+    connector.wipe()
